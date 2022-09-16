8a305b785f82bdf3e81c29a03667cc33e14f2c70
aakansha1234 <20503818+aakansha1234@users.noreply.github.com>
2022-09-16 16:56:53 +0530

    remove deadline conditions for demo

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -21,8 +21,8 @@ contract YourContract is ERC20, Ownable {
   }
 
   receive() external payable {
-    require(nft.ownerOf(id) == address(this));
-    require(block.timestamp >= bidDeadline);
+    // require(nft.ownerOf(id) == address(this));
+    // require(block.timestamp >= bidDeadline);
   }
 
   // this kicks off the entire process.
@@ -40,7 +40,7 @@ contract YourContract is ERC20, Ownable {
 
   // once NFT has been deposited, bidders bid with their ETH
   function bid() external payable {
-    require(block.timestamp < bidDeadline, "over");
+    // require(block.timestamp < bidDeadline, "over");
 
     _mint(msg.sender, msg.value);
   }
@@ -48,7 +48,7 @@ contract YourContract is ERC20, Ownable {
   // Take back bid amount if the bid is still active
   function unbid(uint256 amount) external {
     require(balanceOf(msg.sender) >= amount, "too high");
-    require(block.timestamp < bidDeadline, "over");
+    // require(block.timestamp < bidDeadline, "over");
 
     _burn(msg.sender, amount);
     (bool success, ) = payable(msg.sender).call{value: amount}("");
@@ -57,7 +57,7 @@ contract YourContract is ERC20, Ownable {
 
   // If the total eth raised during the bid is lower than `minBidAmount`, owner can withdraw the NFT.
   function withdrawNFT() external onlyOwner {
-    require(block.timestamp > bidDeadline, "!over");
+    // require(block.timestamp > bidDeadline, "!over");
     require(address(this).balance < minBidAmount, "sold");
 
     nft.safeTransferFrom(address(this), msg.sender, id);
@@ -65,7 +65,7 @@ contract YourContract is ERC20, Ownable {
 
   // If the total eth raised during the bid is higher than `minBidAmount`, owner can withdraw the ETH.
   function withdrawBidETH() external onlyOwner {
-    require(block.timestamp > bidDeadline, "!over");
+    // require(block.timestamp > bidDeadline, "!over");
     require(address(this).balance >= minBidAmount, "!sold");
 
     renounceOwnership();
@@ -79,7 +79,7 @@ contract YourContract is ERC20, Ownable {
   // Holders will vote and if it reaches the majority, it can be executed.
   function proposeCall(address to, bytes calldata data, uint256 deadline) external {
     require(balanceOf(msg.sender) > 0, "0 vote power");
-    require(block.timestamp < deadline, "over");
+    // require(block.timestamp < deadline, "over");
 
     bytes32 hash = keccak256(abi.encodePacked(to, data, deadline));
     require(votes[hash] == 0, "!proposal");
@@ -90,7 +90,7 @@ contract YourContract is ERC20, Ownable {
   // vote on an open proposal (before the deadline)
   function vote(address to, bytes calldata data, uint256 deadline) external {
     require(balanceOf(msg.sender) > 0, "0 votes");
-    require(block.timestamp < deadline, "over");
+    // require(block.timestamp < deadline, "over");
 
     bytes32 hash = keccak256(abi.encodePacked(to, data, deadline));
     require(votes[hash] > 0, "!proposal");
@@ -108,7 +108,7 @@ contract YourContract is ERC20, Ownable {
   // If at least 50% of the tokens have voted, execute the call.
   // this has the potential to transfer the NFT as well, so can be used to integrate with marketplaces or P2P deal.
   function executeCall(address to, bytes calldata data, uint256 deadline) external {
-    require(block.timestamp > deadline, "!over");
+    // require(block.timestamp > deadline, "!over");
 
     bytes32 hash = keccak256(abi.encodePacked(to, data, deadline));
     require(votes[hash] > totalSupply()/2, "!majority");
